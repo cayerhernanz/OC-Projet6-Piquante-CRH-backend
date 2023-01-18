ce537b5dbcc4ce1a6a2f1c66224d25f14006417e
Cayetano <cayetano.rhernanz@gmail.com>
2023-01-18 11:30:06 +0100

    etape 1 ok

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://developper-1:iamthedevelopper1234@ocprojet6crhcluster.4d2xfko.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+module.exports = app;
